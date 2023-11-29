9e1a73794f777f166828933103d1489bc2d71b53
André Marques <andre.m.d.abreu@gmail.com>
2023-11-29 11:18:02 -0300

    Componente único

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,36 @@ function App() {
         É normal ficar entre {} nesta
         situação */}
       <main>
-        <h2>Teste</h2>
+        <section>
+          <h2>Conteúdo da aplicação</h2>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+            maxime eveniet quaerat assumenda natus dolor, excepturi ab corporis
+            perspiciatis fugit quasi aliquam optio cum eos fuga asperiores totam
+            vel reiciendis?
+          </p>
+
+          <article>
+            <h3>Artigo 1</h3>
+            <p>Conteúdo do artigo 1....</p>
+          </article>
+
+          <article>
+            <h3>Artigo 2</h3>
+            <p>Conteúdo do artigo 2....</p>
+          </article>
+
+          <article>
+            <h3>Artigo 3</h3>
+            <p>Conteúdo do artigo 3....</p>
+          </article>
+        </section>
       </main>
+
+      <footer>
+        <h2>Rodapé da aplicação</h2>
+      </footer>
     </>
   );
 }
