d72c48225d4f89965117aa6396e1bc34f0b4b108
André Marques <andre.m.d.abreu@gmail.com>
2023-11-29 11:10:58 -0300

    Formatação com Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 function App() {
-    return <>
-                <header>
-                    <h1>Olá React!</h1>
-                </header>
-                <main>
-                    <h2>Teste</h2>
-                </main>
-            </> 
+  return (
+    <>
+      <header>
+        <h1>Olá React!</h1>
+        <hr />
+        <nav>
+          <a href="">Home</a>
+          <a href="">Produtos</a>
+          <a href="">Contato</a>
+        </nav>
+      </header>
+      {/* Comentário no meio do JS
+        É normal ficar entre {} nesta
+        situação */}
+      <main>
+        <h2>Teste</h2>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
