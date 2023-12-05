2e7d46ce44d3b8c4594074b7fb39b0d30d39c962
André Marques <andre.m.d.abreu@gmail.com>
2023-12-05 09:10:11 -0300

    Uso de template string para classes

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,8 +1,22 @@
 function Rodape() {
+  const exemplosEstilos = {
+    backgroundColor: "lightblue",
+    padding: "2%",
+    color: "blue",
+    border: "solid 4px #f00",
+  };
+
   return (
-    <footer>
-      <h2>Rodapé da aplicação</h2>
+    <footer style={exemplosEstilos}>
+      <h2 style={{ textAlign: "center" }}>Rodapé da aplicação</h2>
+
+      <p className="destaque-texto centralizar">Fundamentos de React</p>
+
+      <p>
+        Experimentando <b className="sombra">classes</b>
+      </p>
     </footer>
   );
 }
+
 export default Rodape;
