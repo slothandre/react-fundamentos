557bbc3d94ff2aa5da863b19cddf141fa3a572f2
André Marques <andre.m.d.abreu@gmail.com>
2023-12-05 09:27:59 -0300

    Firulas no Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,39 @@
+/* Menu.jsx */
+import styled from "styled-components";
+
+const StyledMenu = styled.nav`
+  height: 50px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+
+  a {
+    color: white;
+    text-decoration: none;
+    width: 30%;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: transform 100ms;
+
+    /* & -> referência ao
+    seletor pai (a) */
+    &:hover {
+      background-color: pink;
+      color: black;
+      transform: scale(1.05);
+    }
+  }
+`;
+
 function Menu() {
   return (
-    <nav>
+    <StyledMenu>
       <a href="">Home</a>
       <a href="">Produtos</a>
       <a href="">Contato</a>
-    </nav>
+    </StyledMenu>
   );
 }
 
