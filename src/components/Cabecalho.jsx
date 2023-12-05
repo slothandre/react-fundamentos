7395bc6ed25c297a9fb1aaf4d6afe2f62925016f
André Marques <andre.m.d.abreu@gmail.com>
2023-12-05 09:02:25 -0300

    Componente Cabecalho

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,13 +1,11 @@
+import Menu from "./Menu";
+
 function Cabecalho() {
   return (
     <header>
       <h1>Olá React!</h1>
       <hr />
-      <nav>
-        <a href="">Home</a>
-        <a href="">Produtos</a>
-        <a href="">Contato</a>
-      </nav>
+      <Menu />
     </header>
   );
 }
