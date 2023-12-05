1ebde9d36e48a6d20c98a53340a0a83ea5677ce0
André Marques <andre.m.d.abreu@gmail.com>
2023-12-05 09:27:20 -0300

    Separação do StyledRodape

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,21 +1,10 @@
+import { StyledRodape } from "../styles/styles";
+
 function Rodape() {
-  const exemplosEstilos = {
-    backgroundColor: "lightblue",
-    padding: "2%",
-    color: "blue",
-    border: "solid 4px #f00",
-  };
-
   return (
-    <footer style={exemplosEstilos}>
-      <h2 style={{ textAlign: "center" }}>Rodapé da aplicação</h2>
-
-      <p className="destaque-texto centralizar">Fundamentos de React</p>
-
-      <p>
-        Experimentando <b className="sombra">classes</b>
-      </p>
-    </footer>
+    <StyledRodape>
+      <h2>Rodapé da aplicação</h2>
+    </StyledRodape>
   );
 }
 
