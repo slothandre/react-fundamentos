8af0623cd38ca9704afe5cf8013be1f0b9069410
André Marques <andre.m.d.abreu@gmail.com>
2023-12-05 09:02:37 -0300

    Componente Conteudo

--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -1,3 +1,5 @@
+import Artigo from "./Artigo";
+
 function Conteudo() {
   return (
     <main>
@@ -5,28 +7,19 @@ function Conteudo() {
         <h2>Conteúdo da aplicação</h2>
 
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum maxime
-          eveniet quaerat assumenda natus dolor, excepturi ab corporis
-          perspiciatis fugit quasi aliquam optio cum eos fuga asperiores totam
-          vel reiciendis?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+          labore? Officia quae quo a quas excepturi distinctio sint voluptas
+          labore iste veniam possimus facere adipisci sit repellat, voluptate,
+          expedita aspernatur.
         </p>
 
-        <article>
-          <h3>Artigo 1</h3>
-          <p>Conteúdo do artigo 1....</p>
-        </article>
-
-        <article>
-          <h3>Artigo 2</h3>
-          <p>Conteúdo do artigo 2....</p>
-        </article>
-
-        <article>
-          <h3>Artigo 3</h3>
-          <p>Conteúdo do artigo 3....</p>
-        </article>
+        {/* Reaproveitamento de Componentes */}
+        <Artigo />
+        <Artigo />
+        <Artigo />
       </section>
     </main>
   );
 }
+
 export default Conteudo;
