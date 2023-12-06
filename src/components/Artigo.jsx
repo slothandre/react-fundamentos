0e0a3fc261004a47d9a5e9c65a7ad9100b27bfb1
André Marques <andre.m.d.abreu@gmail.com>
2023-12-06 11:46:50 -0300

    Exercício de useState

--- src/components/Artigo.jsx
+++ src/components/Artigo.jsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const StyledArtigo = styled.article`
-  background-color: lavender;
+  /* background-color: lavender; */
   padding: 1rem;
   margin: 0.5rem 0;
 
@@ -21,14 +22,21 @@ const StyledArtigo = styled.article`
 
 /* Definindo props para o componente */
 function Artigo(props) {
+  const [cor, setCor] = useState("#e6e6fa");
+
+  const mudarCor = () => {
+    cor === "#e6e6fa" ? setCor("#FF0") : setCor("#e6e6fa");
+  };
+
   const formataPreco = (valor) => {
     return valor.toLocaleString("pt-br", {
       style: "currency",
       currency: "BRL",
     });
   };
+
   return (
-    <StyledArtigo>
+    <StyledArtigo onClick={mudarCor} style={{ backgroundColor: cor }}>
       <h3>{props.categoria}</h3>
       <p>
         <b>Curso: </b>
