e7569e3210fa40c41fa74fd03ac35b17ccfa1a55
André Marques <andre.m.d.abreu@gmail.com>
2023-12-07 08:48:30 -0300

    Criação de botões para aplicação de filtros

--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -3,6 +3,46 @@ import Artigo from "./Artigo";
 import styled from "styled-components";
 import cursos from "../api/cursos";
 
+function Conteudo() {
+  const aplicarFiltro = (event) => {
+    console.log(event.currentTarget.innerText);
+  };
+
+  return (
+    <StyledConteudo>
+      <h2>Conteúdo da aplicação</h2>
+
+      <img src="" alt="" />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+        labore? Officia quae quo a quas excepturi distinctio sint voluptas
+        labore iste veniam possimus facere adipisci sit repellat, voluptate,
+        expedita aspernatur.
+      </p>
+
+      <div className="filtros">
+        <p>
+          <b>Filtrar por: </b>
+          <button onClick={aplicarFiltro}>Front-End</button>
+          <button onClick={aplicarFiltro}>Back-End</button>
+          <button onClick={aplicarFiltro}>Design</button>
+        </p>
+      </div>
+
+      <div className="artigos">
+        {cursos.map((curso) => (
+          <Artigo
+            key={curso.id}
+            nome={curso.titulo}
+            categoria={curso.categoria}
+            preco={curso.preco}
+          />
+        ))}
+      </div>
+    </StyledConteudo>
+  );
+}
+
 const StyledConteudo = styled.main`
   width: 90vw;
   margin: 1rem auto;
@@ -13,6 +53,13 @@ const StyledConteudo = styled.main`
   h2,
   p {
     padding: 0.2rem 0;
+  }
+
+  .filtros {
+    margin: 1rem 0;
+    padding: 1rem 0;
+    border-top: solid 2px;
+    border-bottom: solid 2px;
   }
 
   @media screen and (min-width: 650px) {
@@ -35,34 +82,5 @@ const StyledConteudo = styled.main`
     }
   }
 `;
-
-function Conteudo() {
-  const datas = ["10/10/1954", "01/01/1995", "15/12/2012"];
-
-  return (
-    <StyledConteudo>
-      <h2>Conteúdo da aplicação</h2>
-
-      <img src="" alt="" />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
-        labore? Officia quae quo a quas excepturi distinctio sint voluptas
-        labore iste veniam possimus facere adipisci sit repellat, voluptate,
-        expedita aspernatur.
-      </p>
-
-      <div className="artigos">
-        {cursos.map((curso) => (
-          <Artigo
-            key={curso.id}
-            nome={curso.titulo}
-            categoria={curso.categoria}
-            preco={curso.preco}
-          />
-        ))}
-      </div>
-    </StyledConteudo>
-  );
-}
 
 export default Conteudo;
